08d50e8806369b5981f1de036970fd76e2395b3b
yoonhee-song <uni961130@gmail.com>
2022-05-17 20:25:43 +0900

    뷰 리졸버

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,7 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +12,4 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 }
